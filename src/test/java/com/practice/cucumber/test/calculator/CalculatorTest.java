0421217db6030933d2538a606d90c068b34f65bf
Neeraj Jain <jaine03@ca.com>
2017-01-16 14:39:09 +0530

    In Regular Expression for Annotation Replacing \d+ with .+ to support both double and float numbers

--- src/test/java/com/practice/cucumber/test/calculator/CalculatorTest.java
+++ src/test/java/com/practice/cucumber/test/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.practice.cucumber.test.calculator;
 
 import com.practice.cucumber.Calculator;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -8,6 +9,7 @@ import org.junit.Assert;
 
 /**
  * Created by jaine03 on 16/01/17.
+ * In Regular Expression for Annotation Replacing \\d+ with .+ to support both double and float numbers
  */
 public class CalculatorTest {
 
@@ -18,22 +20,22 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @When("^I add (\\d+)$")
+    @When("^I add (.+)$")
     public void addFirst(int number) throws Throwable {
         calculator.add(number);
     }
 
-    @When("^add (\\d+)$")
+    @When("^add (.+)$")
     public void addSecond(int number) throws Throwable {
         calculator.add(number);
     }
 
-    @Then("^result should be (\\d+)$")
+    @Then("^result should be (.+)$")
     public void result_should_be(int result) throws Throwable {
         Assert.assertTrue(calculator.getCurrentValue() == result);
     }
 
-    @When("^subtract (\\d+)$")
+    @When("^subtract (.+)$")
     public void subtract(int number) throws Throwable {
         calculator.subtract(number);
     }
